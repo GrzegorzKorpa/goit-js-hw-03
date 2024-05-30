155f786986298231eee9cbb2cd1d9e62a0c0534d
GrzegorzKorpa <grzegorz.korpa@post.pl>
2024-05-30 18:44:03 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,20 @@
 'use strict';
 
-calculateTotalPrice = (orderedItems) => {
-  let totalPrice = 0; {
+'use strict';
 
-  orderedItems.forEach((item) =>
-    totalPrice += item);}
-    return totalPrice;
-  
-};
+function filterArray(numbers, value) {
+
+  const tags = []; {
+    for (const number of numbers) {
+      if (number > value) { tags.push(`${number}`) }
+    };
+    
+  return tags;}
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
   
